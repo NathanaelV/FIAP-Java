61b1430337c417b9da3e69bbd4516e04c6d758ab
Nathanael Vieira <tmntsplinter04@gmail.com>
2025-08-07 19:44:48 -0300

    Adiciona o Exemplo 03

--- Projeto18/src/br/com/fiap/main/Exemplo03.java
+++ Projeto18/src/br/com/fiap/main/Exemplo03.java
@@ -1,4 +1,21 @@
 package br.com.fiap.main;
 
+import java.util.*;
+
 public class Exemplo03 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<Integer>(
+                Arrays.asList(45,88,13,250,2,99)
+        );
+
+        for (int i = 0; i < numeros.size(); i++) {
+            System.out.print(String.format("%d ", numeros.get(i)));
+        }
+
+        Collections.sort(numeros);
+        System.out.println();
+        for (Integer i : numeros) {
+            System.out.print(String.format("%d ", i));
+        }
+    }
 }
