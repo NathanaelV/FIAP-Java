c994fef65655dcae49b784178f809f305d6bf9fd
Nathanael Vieira <tmntsplinter04@gmail.com>
2025-10-17 10:06:31 -0300

    Add class 16

---
+++ S01Projeto16-Vetores-de-Objetos/src/br/com/fiap/bean/Equipe.java
@@ -0,0 +1,38 @@
+package br.com.fiap.bean;
+
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+public class Equipe {
+    private String nome;
+    private ArrayList<String> integrantes;
+    public Equipe() {}
+
+    public Equipe(String nome, ArrayList<String> integrantes) {
+        this.nome = nome;
+        this.integrantes = integrantes;
+    }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public ArrayList<String> getIntegrantes() {
+        return integrantes;
+    }
+
+    public void setIntegrantes(ArrayList<String> integrantes) {
+        this.integrantes = integrantes;
+    }
+
+    public void listarEquipe() {
+        String exibe = String.format("Nome da equipe: %s \n", nome);
+        int cont = 1;
+
+        for (String i : integrantes) {
+            exibe += String.format("Integrante %d: %s \n", cont, i);
+            cont++;
+        }
+        JOptionPane.showMessageDialog(null, exibe, "Listagem da Equipe", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
