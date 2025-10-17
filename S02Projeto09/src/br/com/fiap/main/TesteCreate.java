378d4108ea6747636bceae2b33e0269ab84a9490
Nathanael Vieira <tmntsplinter04@gmail.com>
2025-10-17 10:12:26 -0300

    Add class S02Projeto09

---
+++ S02Projeto09/src/br/com/fiap/main/TesteCreate.java
@@ -0,0 +1,22 @@
+package br.com.fiap.main;
+
+import br.com.fiap.dao.CarroDAO;
+import br.com.fiap.dao.ConnectionFactory;
+import br.com.fiap.dto.Carro;
+
+import java.sql.Connection;
+
+public class TesteCreate {
+    public static void main(String[] args) {
+        Connection con = ConnectionFactory.abrirConexao();
+        CarroDAO carroDAO = new CarroDAO(con);
+
+        Carro carro = new Carro();
+        carro.setPlaca("JKK1900");
+        carro.setCor("Vermelho");
+        carro.setDescricao("Nissan Kicks");
+
+        System.out.println(carroDAO.inserir(carro));
+        ConnectionFactory.fecharConexao(con);
+    }
+}
